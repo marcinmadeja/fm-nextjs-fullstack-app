56d842afd6343a7a3174a8d753092a398ba325ca
Marcin Madeja <madeja.m.b@gmail.com>
2022-03-19 12:35:56 +0100

    Initial commit

---
+++ components/playerLayout.tsx
@@ -0,0 +1,28 @@
+import { Box } from '@chakra-ui/layout'
+import Sidebar from './sidebar'
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        width="250px"
+        height="calc(100% - 100px)"
+      >
+        <Sidebar />
+      </Box>
+
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+
+      <Box position="absolute" bottom="0" left="0">
+        player
+      </Box>
+    </Box>
+  )
+}
+
+export default PlayerLayout
---
+++ components/sidebar.tsx
@@ -0,0 +1,140 @@
+import NextImage from 'next/image'
+import NextLink from 'next/link'
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/layout'
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from 'react-icons/md'
+
+const navMenu = [
+  {
+    name: 'Home',
+    icon: MdHome,
+    route: '/',
+  },
+  {
+    name: 'Search',
+    icon: MdSearch,
+    route: '/search',
+  },
+  {
+    name: 'Your Library',
+    icon: MdLibraryMusic,
+    route: '/library',
+  },
+]
+
+const musicMenu = [
+  {
+    name: 'Create Playlist',
+    icon: MdPlaylistAdd,
+    route: '/',
+  },
+  {
+    name: 'Favorites',
+    icon: MdFavorite,
+    route: '/favorites',
+  },
+]
+
+const playlists = new Array(30)
+  .fill(1)
+  .map((_, i) => ({ id: `Playlist ${i + 1}`, name: `Playlist ${i + 1}` }))
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100%)"
+      bg="black"
+      paddingX="5px"
+      color="grey"
+    >
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" paddingX="20px">
+          <NextImage src="/logo.svg" width="120" height="60" />
+        </Box>
+
+        <Box margin="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        <Box marginTop="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        <Divider color="gray.800" />
+
+        <Box height="66%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist.id}>
+                <LinkBox>
+                  <NextLink
+                    href={{
+                      pathname: '/playlist/[id]',
+                      query: { id: playlist.id },
+                    }}
+                    passHref
+                  >
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Sidebar
